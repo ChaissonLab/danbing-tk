56141620e5deb020b9551466c653b3d23113f416
Tsung-Yu Lu <tsungyul@usc.edu>
2018-11-19 16:51:18 -0800

    remove old query approach

--- extractLoci.sh
+++ extractLoci.sh
@@ -1,19 +1,61 @@
-#flist=("ERR899717_1.fastq" "ERR899717_2.fastq" "ERR903031_1.fastq" "ERR903031_2.fastq" "GM19240_NoIndex_L008_R1_001.fastq" "GM19240_NoIndex_L008_R2_001.fastq")
-flist=("HG00514" "HG00733" "NA19240")
-fsize=${#flist[@]}
-loci=-1
-# max = 5349
-max=9
-for ((ind=0; ind<$fsize; ind++)); do
-	file=${flist[$ind]}
-	if [ ! -f "$file"".L0.kmercount" ]; then
-		awk -v LL="$loci" -v ff="$file" -v max="$max" '{ if (substr($1, 0, 1) == ">") { LL += 1; start = 1; if (LL == max+1) {exit} } else { if (start == 1) {print >>  ff".L"LL".kmercount"} } }' "$file"".21.kmers"
-		for i in `seq 0 $max`; do
-			sort "$file"".L""$i"".kmercount" | awk '{print $2}' >> "$file"".L""$i"".sort.count"
-			# sort "$file"".L""$i"".kmercount" | awk '{print $1}' >> "$file"".L""$i"".sort.kmerstring"
-			echo "$i done"
-		done
-	else
-		echo "file already exists!"
-	fi
+# arg1: haplotype file name e.g. PanGenomeGenotyping.21.1950.kmers or ERR899717_1.fastq.21.kmers
+# arg2: loci e.g."all" or "big or ""beg4870" or "itv100,500" or "end4870" or "4870"
+
+
+nloci=( $(( $(awk '{ if (substr($0,0,1) == ">") {c++} } END {print c}' "$1") - 1)) )
+lociList=()
+if [ "$2" == "all" ]; then
+    lociList=($(seq 0 1 $nloci))
+    echo "extracting all loci"
+elif [[ "$2" == "beg"* ]]; then
+    beg=$2
+    beg=${beg:3}
+    lociList=($(seq $beg 1 $nloci))
+    echo "extracting loci starting from: ""${lociList[0]}"
+elif [[ "$2" == "end"* ]]; then
+    end=$2
+    end=${end:3}
+    lociList=($(seq 0 1 $end))
+    echo "extracting loci till: ""${lociList[0]}"
+elif [[ "$2" == "itv"* ]]; then
+    itv=$2
+    itv=${itv:3}
+    itv=(${itv//,/ })
+    lociList=($(seq ${itv[0]} 1 ${itv[1]}))
+    echo "extracting loci from: ""${lociList[0]}"" to ""${lociList[-1]}"
+elif [[ "$2" == "big" ]]; then
+    awk 'BEGIN {loci = -1} 
+        {
+            if (substr($0,0,1) == ">") {
+                if (loci != -1) {print loci, c}
+                c = 0;
+                loci += 1
+            } else {c += $2} 
+        } 
+        END {print loci, c}' "$1" > "$1"".loci.count"
+    lociList=($(sort -nk2,2 "$1"".loci.count" | tail | awk '{print $1}'))
+    echo "extracting loci: ""${lociList[@]}"
+else
+    num=$(($#-1))
+    lociList=( ${@:2:$num} )
+    echo "extracting loci: ""${lociList[@]}"
+fi
+
+lsize=${#lociList[@]}
+out=$(basename -- "$1")
+ii=-1
+for ((ind=0; ind<$lsize; ind++)); do
+    loci=${lociList[$ind]}
+    if [ ! -f "$out"".L"$loci".sort.count" ]; then
+        awk -v II="$ii" -v LL="$loci" -v oo="$out" '{ if (substr($1, 0, 1) == ">") { II += 1; if (II == LL) {start = 1}; if (II == LL+1) {exit} } else { if (start == 1) {print >> oo".L"LL".kmercount"} } }' "$1"
+        if [ -f "$out"".L"$loci".kmercount" ]; then
+            sort "$out"".L""$loci"".kmercount" | awk '{print $2}' >> "$out"".L""$loci"".sort.count"
+            echo "$loci done"
+        else
+            echo "$loci is empty"
+        fi
+    else
+        echo "file already exists!"
+    fi
 done
+
